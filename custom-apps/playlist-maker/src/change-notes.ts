1a4e8f983e21052d49a4db4b5ad50eb69d7f8b79
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-09-17 19:47:29 +0200

    chore: update app versions

--- custom-apps/playlist-maker/src/change-notes.ts
+++ custom-apps/playlist-maker/src/change-notes.ts
@@ -1,1 +1,9 @@
-export const CHANGE_NOTES = ['Fixed a modal preventing access to the app'];
+export const CHANGE_NOTES = [
+    'Added a new result node to directly add the result to a playlist',
+    'Added a new source node (playlist recommended tracks)',
+    'Added a new processing node (subset)',
+    'Added two new filter nodes (is explicit and is saved)',
+    'Moved the result count to the topbar',
+    'Added a loading indicator to dropdowns',
+    'Renamed the "Relative complement" node to "Substract" for clarity (If you have saved workflow with this node, the node will be blank and you will need to replace it)',
+];
